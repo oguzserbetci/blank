ecc974bcdd77ac38ff1dde67aacc4ee7815c566c
Oguz Serbetci <oguz.serbetci@gmail.com>
2019-05-27 23:19:37 +0200

    Add prepositions.

--- content.js
+++ content.js
@@ -1,10 +1,10 @@
-var determiners = ['(d[ei][rmnse])', '(ein[e]*[nsmr]*)']
-var types = ['bestimmt', 'unbestimmt']
+var rxs = ['(d[ei][rmnse])', '(ein[e]*[nsmr]*)', '(beim?|a[nm]|auf|i[nm]|vo[nm]|über|zu[mr]?|nach|aus|durch|um)']
+var types = ['bestimmt', 'unbestimmt', 'preposition']
 
 var paragraphs = document.getElementsByTagName("p")
 for (var i = 0, l = paragraphs.length; i < l; i++) {
-    var random_ind = Math.floor(Math.random() * determiners.length)
-    var reg_str = '\\b' + determiners[random_ind] + '\\b'
-    var myExp = new RegExp(reg_str, 'im')
-    paragraphs[i].innerHTML = paragraphs[i].innerHTML.replace(myExp, "<span class='" + types[random_ind] + " blank'>$1</span>")
+    var random_ind = Math.floor(Math.random() * rxs.length)
+    var rx_str = '\\b' + rxs[random_ind] + '\\b'
+    var rx = new RegExp(rx_str, 'igm')
+    paragraphs[i].innerHTML = paragraphs[i].innerHTML.replace(rx, "<span class='" + types[random_ind] + " blank'>$1</span>")
 }
