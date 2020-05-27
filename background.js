a5f29163587a419758d45d8b2602316f50a0f183
Oğuz Şerbetçi <oguz.serbetci@gmail.com>
2020-05-28 01:36:11 +0200

    Add blanks with pressing on toolbar icon.

---
+++ background.js
@@ -0,0 +1,15 @@
+browser.browserAction.onClicked.addListener(() => {
+  browser.tabs.query({
+    currentWindow: true,
+    active: true
+  }).then(sendMessageToTabs);
+});
+
+function sendMessageToTabs(tabs) {
+  for (let tab of tabs) {
+    browser.tabs.sendMessage(
+    tab.id,
+    {greeting: "Hi from background script"}
+    );
+  }
+}
--- content.js
+++ content.js
@@ -1,26 +1,30 @@
-browser.storage.local.get("rxs").then(results => {
-    var rxs = results["rxs"]
+browser.runtime.onMessage.addListener(blankizePage);
 
-    var paragraphs = document.getElementsByTagName("p")
-    for (var i = 0; i < paragraphs.length; i++) {
-        var random_ind = Math.floor(Math.random() * rxs.length)
-        var rx_str = '\\b' + rxs[random_ind].rx + '(?=\\s|$)'
-        var regex = new RegExp(rx_str, 'igm')
+function blankizePage() {
+    browser.storage.local.get("rxs").then(results => {
+        var rxs = results["rxs"]
+        
+        var paragraphs = document.getElementsByTagName("p")
+        for (var i = 0; i < paragraphs.length; i++) {
+            var random_ind = Math.floor(Math.random() * rxs.length)
+            var rx_str = '\\b' + rxs[random_ind].rx + '(?=\\s|$)'
+            var regex = new RegExp(rx_str, 'igm')
 
-        const paragraph = paragraphs[i]
-        var node = paragraph.firstChild
-        do {
-            if (node.nodeType === Node.TEXT_NODE) {
-                while ((match = regex.exec(node.textContent)) != null) {
-                    node = node.splitText(match.index)
-                    node.textContent = node.textContent.substring(match[1].length)
-                    var blankNode = document.createElement("span")
-                    blankNode.textContent = match[1]
-                    blankNode.style.cssText = "background-color:" + rxs[random_ind].color + "; width:5em"
-                    blankNode.className = "blank"
-                    paragraph.insertBefore(blankNode, node)
+            const paragraph = paragraphs[i]
+            var node = paragraph.firstChild
+            do {
+                if (node.nodeType === Node.TEXT_NODE) {
+                    while ((match = regex.exec(node.textContent)) != null) {
+                        node = node.splitText(match.index)
+                        node.textContent = node.textContent.substring(match[1].length)
+                        var blankNode = document.createElement("span")
+                        blankNode.textContent = match[1]
+                        blankNode.style.cssText = "background-color:" + rxs[random_ind].color + "; width:5em"
+                        blankNode.className = "blank"
+                        paragraph.insertBefore(blankNode, node)
+                    }
                 }
-            }
-        } while (node = node.nextSibling);
-    }
-})
+            } while (node = node.nextSibling);
+        }
+    })
+}
