26f360e108a7b6f1cc63f0d0d780b8571c076c89
Oğuz Şerbetçi <oguz.serbetci@gmail.com>
2020-05-29 12:05:08 +0200

    Allow clicking and storing correct guessing

--- content.js
+++ content.js
@@ -1,5 +1,30 @@
 browser.runtime.onMessage.addListener(blankizePage);
 
+function storeCorrect(event) {
+    const elem = event.target
+    
+    elem.className += " correct"
+    elem.parentElement.children[0].className += " correct"
+    
+    console.log('clicked')
+    browser.storage.local.get("scores").then(res => {
+        if (res['scores']) {
+            var scores = res['scores']
+            score = scores[elem.parentElement.children[0].textContent] ? scores[elem.parentElement.children[0].textContent] : 0;
+            score += 1
+            scores[elem.parentElement.children[0].textContent] = score
+            browser.storage.local.set({scores});
+            console.log(res['scores'])
+        } else {
+            var scores = {}
+            scores[elem.parentElement.children[0].textContent] = 1
+            browser.storage.local.set({scores});
+        }
+        
+    })
+
+}
+
 function blankizePage() {
     browser.storage.local.get("regexes").then(results => {
         const regexes = results["regexes"].filter(regex => regex.isActive)
@@ -7,27 +32,32 @@ function blankizePage() {
         var paragraphs = document.getElementsByTagName("p")
         for (var i = 0; i < paragraphs.length; i++) {
             const random_ind = Math.floor(Math.random() * regexes.length)
-            const regex = regexes[random_ind].rx
+            const regex = regexes[random_ind]
 
             const paragraph = paragraphs[i]
             var node = paragraph.firstChild
             do {
                 if (node.nodeType === Node.TEXT_NODE) {
-                    while ((match = regex.exec(node.textContent)) != null) {
+                    while ((match = regex.rx.exec(node.textContent)) != null) {
                         node = node.splitText(match.index)
                         node.textContent = node.textContent.substring(match[1].length)
+
                         const container = document.createElement("span")
                         container.className = "blank-container"
-                        container.style.cssText = "background-color:" + regexes[random_ind].color + ";"
                         paragraph.insertBefore(container, node)
+
+                        const subNode = document.createElement("span")
+                        subNode.textContent = regex.substr
+                        subNode.className = "sub"
+                        subNode.style.cssText = "background-color:" + regex.color + ";"
+                        container.appendChild(subNode)
+
                         const blankNode = document.createElement("span")
                         blankNode.textContent = match[1]
                         blankNode.className = "blank"
+                        blankNode.addEventListener("click", storeCorrect)
+
                         container.appendChild(blankNode)
-                        const subNode = document.createElement("span")
-                        subNode.textContent = regexes[random_ind].substr
-                        subNode.className = "sub"
-                        container.appendChild(subNode)
                     }
                 }
             } while (node = node.nextSibling);
